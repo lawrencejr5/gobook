c7a7a60c74aeefb225e4bf1de2ec39b83147167f
Oputa Lawrence <oputalawrence@gmail.com>
2023-11-13 09:53:44 +0100

    Update conn.php
    Changed values of some variables

--- gobook/backend/conn.php
+++ gobook/backend/conn.php
@@ -5,13 +5,10 @@ class Connection{
     protected $conn;
 
     public function __construct(){
-        // $dbhost = "localhost:3325";
-        $dbhost = "localhost";
-        // $dbuser = "root";
-        $dbuser = "gobookn3_lawjun";
-        // $dbpass = "";
-        $dbpass = "Law3221211#$*";
-        $dbname = "gobookn3_database";
+        $dbhost = "localhost:3325";
+        $dbuser = "root";
+        $dbpass = "";
+        $dbname = "gobook_db";
 
         try{
             $this -> conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
